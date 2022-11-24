b3f5988b450ecb2cb1e7440607d1660290be2233
alperenkursun <alperenkursun@outlook.com>
2022-11-24 09:16:14 +0300

    project added

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <pre>
+        Created by <a href="https://github.com/alperenkursun">Alperen Kurşun</a>
+      </pre>
+    </div>
+  );
+}
+
+export default Footer;
